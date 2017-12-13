0eb9324ffae1d59b9483e29f0174cff89d744e3d
DK\ili <ili@sitecore.net>
2017-12-13 12:17:02 +0200

    Original Code

---
+++ src/Sitecore.Support.125069/Sitecore/EmailCampaign/ExperienceAnalytics/Reporting/AbTestReportingClient.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.CSharp.RuntimeBinder;
+using Sitecore.EmailCampaign.ExperienceAnalytics.Properties;
+using Sitecore.ExM.Framework.Diagnostics;
+using Sitecore.ExperienceAnalytics.Api;
+using Sitecore.ExperienceAnalytics.Api.Response;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
+using Sitecore.EmailCampaign.ExperienceAnalytics.Reporting;
+
+namespace Sitecore.EmailCampaign.ExperienceAnalytics.Reporting
+{
+  #region Original Code
+  public class AbTestReportingClient : ReportingClientBase<AbTestReportingStatistics>
+  {
+    
+    public AbTestReportingClient() : base(ApiContainer.Repositories.GetReportingService(), Settings.Default.ByAbTestVariantSegmentId)
+    {
+    }
+   
+  }
+  #endregion Original Code
+}
