68423088281446bf7049d3a354e2b2724b8278e3
DK\ili <ili@sitecore.net>
2017-12-13 12:34:03 +0200

    Release

--- src/Sitecore.Support.125069/Sitecore/EmailCampaign/ExperienceAnalytics/Reporting/AbTestReportingClient.cs
+++ src/Sitecore.Support.125069/Sitecore/EmailCampaign/ExperienceAnalytics/Reporting/AbTestReportingClient.cs
@@ -1,24 +1,30 @@
-﻿using Microsoft.CSharp.RuntimeBinder;
-using Sitecore.EmailCampaign.ExperienceAnalytics.Properties;
-using Sitecore.ExM.Framework.Diagnostics;
-using Sitecore.ExperienceAnalytics.Api;
-using Sitecore.ExperienceAnalytics.Api.Response;
+﻿using Sitecore.EmailCampaign.ExperienceAnalytics.Reporting;
 using System;
-using System.Collections.Generic;
-using System.Linq.Expressions;
-using System.Runtime.CompilerServices;
-using Sitecore.EmailCampaign.ExperienceAnalytics.Reporting;
+using System.Reflection;
 
-namespace Sitecore.EmailCampaign.ExperienceAnalytics.Reporting
+namespace Sitecore.Support.EmailCampaign.ExperienceAnalytics.Reporting
 {
   #region Original Code
-  public class AbTestReportingClient : ReportingClientBase<AbTestReportingStatistics>
-  {
-    
-    public AbTestReportingClient() : base(ApiContainer.Repositories.GetReportingService(), Settings.Default.ByAbTestVariantSegmentId)
-    {
-    }
-   
-  }
+  //public class AbTestReportingClient : ReportingClientBase<AbTestReportingStatistics>
+  //{
+
+  //  public AbTestReportingClient() : base(ApiContainer.Repositories.GetReportingService(), Settings.Default.ByAbTestVariantSegmentId)
+  //  {
+  //  }
+
+  //}
   #endregion Original Code
+
+  #region New Code
+  public class AbTestReportingClient : Sitecore.EmailCampaign.ExperienceAnalytics.Reporting.AbTestReportingClient
+  {
+    public AbTestReportingClient()
+    {
+      typeof(ReportingClientBase<>).MakeGenericType(new Type[]
+      {
+                typeof(AbTestReportingStatistics)
+      }).GetField("_service", BindingFlags.Instance | BindingFlags.NonPublic).SetValue(this, new SupportReportingService());
+    }
+  }
+  #endregion New Code
 }
---
+++ src/Sitecore.Support.125069/Sitecore/EmailCampaign/ExperienceAnalytics/Reporting/SupportReportingService.cs
@@ -0,0 +1,68 @@
+﻿using Sitecore.Analytics.Reporting;
+using Sitecore.ExperienceAnalytics.Api;
+using Sitecore.ExperienceAnalytics.Api.Query;
+using Sitecore.ExperienceAnalytics.Api.Response;
+using System;
+using System.Reflection;
+
+namespace Sitecore.Support.EmailCampaign.ExperienceAnalytics.Reporting
+{
+  internal class SupportReportingService : IReportingService
+  {
+    private static object reportingService;
+
+    private static object aggregationSegmentReader;
+
+    private static object dimensionDefinitionService;
+
+    private static object segmentReader;
+
+    private static object reportDataService;
+
+    private static MethodInfo mGetData;
+
+    private static MethodInfo mExecuteQuery;
+
+    private static Type tLiveReportResponseBuilder;
+
+    private static MethodInfo mGetReportResponse;
+
+    static SupportReportingService()
+    {
+      SupportReportingService.reportingService = ApiContainer.Repositories.GetReportingService();
+      SupportReportingService.aggregationSegmentReader = typeof(ReportQuery).Assembly.GetType("Sitecore.ExperienceAnalytics.Api.ReportingService").GetField("aggregationSegmentReader", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(SupportReportingService.reportingService);
+      SupportReportingService.dimensionDefinitionService = typeof(ReportQuery).Assembly.GetType("Sitecore.ExperienceAnalytics.Api.ReportingService").GetField("dimensionDefinitionService", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(SupportReportingService.reportingService);
+      SupportReportingService.segmentReader = typeof(ReportQuery).Assembly.GetType("Sitecore.ExperienceAnalytics.Api.ReportingService").GetField("segmentReader", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(SupportReportingService.reportingService);
+      SupportReportingService.reportDataService = typeof(ReportQuery).Assembly.GetType("Sitecore.ExperienceAnalytics.Api.ReportingService").GetField("reportDataService", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(SupportReportingService.reportingService);
+      SupportReportingService.mGetData = typeof(ReportQuery).Assembly.GetType("Sitecore.ExperienceAnalytics.Api.Query.QueryData.ReportQueryExtensions").GetMethod("GetData", new Type[]
+      {
+                typeof(ReportQuery)
+      });
+      SupportReportingService.mExecuteQuery = typeof(ReportQuery).Assembly.GetType("Sitecore.ExperienceAnalytics.Api.ReportDataService").GetMethod("ExecuteQuery");
+      SupportReportingService.tLiveReportResponseBuilder = typeof(ReportQuery).Assembly.GetType("Sitecore.ExperienceAnalytics.Api.Response.ReportResponseData.LiveReportResponseBuilder");
+      SupportReportingService.mGetReportResponse = SupportReportingService.tLiveReportResponseBuilder.GetMethod("GetReportResponse");
+    }
+
+    public ReportResponse RunQuery(ReportQuery reportQuery)
+    {
+      object obj = SupportReportingService.mGetData.Invoke(reportQuery, new object[]
+      {
+                reportQuery
+      });
+      object obj2 = SupportReportingService.mExecuteQuery.Invoke(SupportReportingService.reportDataService, new object[]
+      {
+                obj,
+                CachingPolicy.WithCacheDisabled
+      });
+      object obj3 = SupportReportingService.tLiveReportResponseBuilder.GetConstructors()[0].Invoke(new object[]
+      {
+                SupportReportingService.dimensionDefinitionService,
+                SupportReportingService.aggregationSegmentReader,
+                SupportReportingService.segmentReader,
+                obj,
+                obj2
+      });
+      return (ReportResponse)SupportReportingService.mGetReportResponse.Invoke(obj3, new object[0]);
+    }
+  }
+}
